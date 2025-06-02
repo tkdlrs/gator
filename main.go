2897d37cb8b0cc8ab9f023f0f09ad566246bdd48
tkdlrs <tkd.lsanchez@gmail.com>
2025-06-02 12:07:56 -0600

    First rough attempt at figuring out what  in  of the  guided project is asking for.

--- main.go
+++ main.go
@@ -1,27 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/tkdlrs/gator/internal/config"
 )
 
 func main() {
+	// cfg, err := config.Read()
+	// if err != nil {
+	// 	log.Fatalf("error reading config: %v", err)
+	// }
+	// fmt.Printf("Read config: %+v\n", cfg)
+	// //
+	// err = cfg.SetUser("levi")
+	// if err != nil {
+	// 	log.Fatalf("Could not set current user: %v", err)
+	// }
+	// //
+	// cfg, err = config.Read()
+	// if err != nil {
+	// 	log.Fatalf("error reading config: %v", err)
+	// }
+	// fmt.Printf("Read config again: %+v\n", cfg)
+
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Printf("Read config: %+v\n", cfg)
-	//
-	err = cfg.SetUser("levi")
-	if err != nil {
-		log.Fatalf("Could not set current user: %v", err)
+	state := State{&cfg}
+
+	theCommands := commands{
+		"login": handlerLogin,
 	}
-	//
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
-	}
-	fmt.Printf("Read config again: %+v\n", cfg)
 }
