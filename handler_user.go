df560762de533df69d67dc79365c045995a2c1a4
tkdlrs <tkd.lsanchez@gmail.com>
2025-06-07 16:49:45 -0600

    My version (without viewing solution) for 'Chapter 2: Database, Lesson 4: Reset' in the Blog aggregator guided project.

--- handler_user.go
+++ handler_user.go
@@ -60,3 +60,13 @@ func printUser(user database.User) {
 	fmt.Printf("	* ID:		%v\n", user.ID)
 	fmt.Printf("	* Name:		%v\n", user.Name)
 }
+
+func handlerReset(s *state, cmd command) error {
+	err := s.db.DeleteUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("unable to delete all users: %v", err)
+	}
+	//
+	fmt.Println("Successfully removed all users")
+	return nil
+}
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -12,3 +12,8 @@ RETURNING *;
 SELECT * 
 FROM users 
 WHERE name = $1;
+
+
+-- name: DeleteUsers :exec 
+DELETE FROM users; 
+
