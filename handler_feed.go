4e5b685f4a36efad7ca36494ea9e14254f036ba4
tkdlrs <tkd.lsanchez@gmail.com>
2025-06-13 19:07:41 -0600

    Chapter 4: Following, Lesson 1: Follow. I needed to use a Seer Stone and look at the solution files to get through this one. The SQL kept tripping me up.

--- handler_feed.go
+++ handler_feed.go
@@ -35,8 +35,23 @@ func handlerAddFeed(s *state, cmd command) error {
 		return fmt.Errorf("couldn't create a feed: %w", err)
 	}
 	//
+	feedFollowsArgs := database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID:    user.ID,
+		FeedID:    feed.ID,
+	}
+	feedFollow, err := s.db.CreateFeedFollow(context.Background(), feedFollowsArgs)
+	if err != nil {
+		return fmt.Errorf("couldn't create feed follow: %w", err)
+	}
+	//
 	fmt.Println("Feed created successfully:")
 	printFeed(feed, user)
+	fmt.Println()
+	fmt.Println("Feed followed successfully:")
+	printFeedFollow(feedFollow.UserName, feedFollow.FeedName)
 	fmt.Println("==================================")
 	//
 	return nil
---
+++ handler_feed_follows.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/tkdlrs/gator/internal/database"
+)
+
+func handlerFollow(s *state, cmd command) error {
+	// Get the ID of the current user.
+	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return fmt.Errorf("couldn't find user: %w", err)
+	}
+	//
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <url>", cmd.Name)
+	}
+	// Get the ID of the current Feed based on the URL
+	url := cmd.Args[0]
+	feed, err := s.db.GetFeedByURL(context.Background(), url)
+	if err != nil {
+		return fmt.Errorf("couldn't find feed: %w", err)
+	}
+	// create a new feed follow record for current user.
+	feedFollowArgs := database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID:    user.ID,
+		FeedID:    feed.ID,
+	}
+	feedFollowsRow, err := s.db.CreateFeedFollow(context.Background(), feedFollowArgs)
+	if err != nil {
+		return fmt.Errorf("couldn't create feed follow: %w", err)
+	}
+	// Print the name of the feed and current users
+	fmt.Println("Feed follow created:")
+	printFeedFollow(feedFollowsRow.UserName, feedFollowsRow.FeedName)
+	//
+	return nil
+}
+
+func handlerListFeedFollows(s *state, cmd command) error {
+	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return err
+	}
+	//
+	feedFollows, err := s.db.GetFeedFollowsForUser(context.Background(), user.ID)
+	if err != nil {
+		return fmt.Errorf("couldn't get feed follows: %w", err)
+	}
+	//
+	if len(feedFollows) == 0 {
+		fmt.Println("No feed follows found for this user.")
+		return nil
+	}
+	//
+	fmt.Printf("Feed follows for user %s:\n", user.Name)
+	for _, ff := range feedFollows {
+		fmt.Printf("* %s\n", ff.FeedName)
+	}
+	//
+	return nil
+}
+
+func printFeedFollow(username, feedname string) {
+	fmt.Printf("* User:          %s\n", username)
+	fmt.Printf("* Feed:          %s\n", feedname)
+}
